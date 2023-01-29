a27e14d6d36ef3e3516695cdfc95fde18b50e1cf
Aleksander Kowalski <mrhdolek14@gmail.com>
2023-01-29 14:22:25 +0100

    fix: change service provider

--- src/MeetupServiceProvider.php
+++ src/MeetupServiceProvider.php
@@ -4,27 +4,67 @@ declare(strict_types=1);
 
 namespace Blumilk\Meetup\Core;
 
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Blumilk\Meetup\Core\Http\Routing\WebRouting;
+use Blumilk\Meetup\Core\Traits\PublishesMigrations;
+use Illuminate\Support\ServiceProvider;
 
-class MeetupServiceProvider extends PackageServiceProvider
+class MeetupServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    use PublishesMigrations;
+    public function boot(): void
     {
-        $package
-            ->name("meetup")
-            ->hasConfigFile("*")
-            ->hasViews()
-            ->hasAssets()
-            ->publishesServiceProvider("MeetupServiceProvider")
-            ->hasRoute("web")
-            ->hasMigration("*")
-            ->hasInstallCommand(function (InstallCommand $command): void {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->copyAndRegisterServiceProviderInApp();
-            });
+
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . "/../resources/views" => resource_path("views"),
+            ], "views");
+
+            $this->publishes([
+                dirname(__DIR__) . "/database/seeders" => database_path("seeders"),
+            ], "seeders");
+
+            $this->publishes([
+                __DIR__ . "/../resources/static" => public_path("vendor/meetup"),
+            ], "assets");
+
+            $this->publishMigrations();
+            $this->publishConfigs();
+        }
+    }
+
+    public function register(): void
+    {
+        $this->registerConfigs();
+        $this->registerRoutes();
+    }
+
+    protected function registerConfigs(): void
+    {
+        $configs = array_slice(scandir(__DIR__ . "/../config"), 2);
+
+        foreach ($configs as $config) {
+            $this->mergeConfigFrom(
+                __DIR__ . "/../config/" . $config,
+                basename($config, ".php"),
+            );
+        }
+    }
+
+    protected function publishConfigs(): void
+    {
+        $configs = array_slice(scandir(__DIR__ . "/../config"), 2);
+
+        foreach ($configs as $config) {
+            $this->publishes([
+                __DIR__ . "/../config/" . $config,
+                basename($config, ".php")],'config'
+            );
+        }
+    }
+
+    protected function registerRoutes(): void
+    {
+        $this->app->get(WebRouting::class);
     }
 }
